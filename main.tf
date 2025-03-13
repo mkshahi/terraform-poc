ec66abc6aeca9dd56ba50aeed7bae259bbeb7570
mkshahi <mkshahi2004@gmail.com>
2025-03-13 20:49:48 +0530

    make subnet id as list

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "database" {
     db_name = var.db_name
   db_username = var.db_username
   db_password = var.db_password
-  subnet_ids = module.network.private_subnet_id
+  subnet_ids = [module.network.private_subnet_id]
   
 }
 
