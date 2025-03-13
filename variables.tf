18ef5d4969248f0daad2c7fec5730ef92460fd2b
mkshahi <mkshahi2004@gmail.com>
2025-03-13 17:28:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,6 +15,12 @@ variable "public_subnet_cidr" {
   default = "10.0.1.0/24"
 }
 
+variable "private_subnet_cidr" {
+  
+  default = "10.0.2.0/24"
+}
+
+
 variable "instance_type" {
   default = "t2.micro"
   
